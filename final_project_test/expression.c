04049e669a98cee2361dca299bff5512cc0de5fc
周秉頡 <b09902036@csie.ntu.edu.tw>
2021-06-06 15:19:34 +0800

    aa

--- final_project_test/expression.c
+++ final_project_test/expression.c
@@ -5,6 +5,16 @@
 eval
 isToken
 */
+int StrIndex=0;
+char allString[4096];
+
+typedef struct expression
+{
+    //type{0:token 1:operater}
+    char *data;
+    int type;
+}exp;
+
 
 bool isToken(char c)
 {
@@ -14,7 +24,19 @@ bool isToken(char c)
     return false;
 }
 
+void parse(char *str)
+{
+
+}
+
 int main()
 {
-    char *testString="asdjkfn | asfd";
+    char testString[20];
+    char *str2=&(testString[8]);
+    for(int i=0;i<19;i++)testString[i]='a';
+    testString[7]='\0';
+    testString[19]='\0';
+    scanf("%s",testString);
+    printf("%s\n",testString);
+    printf("%s",str2);
 }
