c3c50a5a885fd219683a42efc214bcf1873520b1
周秉頡 <b09902036@csie.ntu.edu.tw>
2021-06-06 15:53:48 +0800

    <expression change>

--- final_project_test/expression.c
+++ final_project_test/expression.c
@@ -8,12 +8,100 @@ isToken
 int StrIndex=0;
 char allString[4096];
 
-typedef struct expression
+// typedef struct expression
+// {
+//     //type{0:token 1:operater}
+//     char *data;
+//     int type;
+//     //result{-1:uneval,0:false,1:true}
+//     int result;
+// }exp;
+typedef struct stack
 {
-    //type{0:token 1:operater}
     char *data;
-    int type;
-}exp;
+    int top;
+}stack;
+
+
+
+// ==========vector===========
+#define maxlength 100
+typedef struct vector
+{
+    int *data;
+    int now,len;
+    struct vector*next,*prev,*back;
+}vec;
+
+vec new_vec()
+{
+    vec *v=malloc(sizeof(vec));
+    v->data=malloc(maxlength*sizeof(int));
+    v->len=maxlength;v->now=0;
+    v->prev=NULL;v->next=NULL;
+    v->back=NULL;
+    return *v;
+}
+
+void vec_push(vec*v,int value)
+{
+    vec *back=v->back;
+    if(value==255)
+    {
+        value++;
+        value--;
+    }
+    if(back==NULL)
+    {
+        back=v;
+    }
+    if(back->now<back->len)
+    {
+        back->data[back->now++]=value;
+        return ;
+    }
+    //else
+    int *datatemp=(int*)realloc(back->data,sizeof(int)*(back->len+maxlength));
+    
+    if(datatemp==NULL)
+    {
+        vec next=new_vec();
+        back->next=&next;
+        next.prev=back;
+        v->back=&next;
+        next.data[next.now++]=value;
+    }
+    else
+    {
+        back->data=datatemp;
+        back->len+=maxlength;
+        back->data[back->now++]=value;
+    }
+    datatemp=NULL;
+}
+int* vec_at(vec*v,int index)
+{
+    if(index==255)
+    {
+        index++;
+        index--;
+    }
+    vec *temp=v;
+    while(1)
+    {
+        if(temp==NULL)
+        {
+            return NULL;
+        }
+        if(index<temp->now)
+        {
+            return &(temp->data[index]);
+        }
+        index-=temp->len;
+        temp=temp->next;
+    }
+}
+//========================
 
 
 bool isToken(char c)
@@ -26,7 +114,10 @@ bool isToken(char c)
 
 void parse(char *str)
 {
-
+    for(int i=0;str[i]!='\0';i++)
+    {
+        
+    }
 }
 
 int main()
